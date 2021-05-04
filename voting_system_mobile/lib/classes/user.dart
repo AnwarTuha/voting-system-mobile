47d553cb509e0a1e28977809a462b74af0934552
anwartuha2 <anwartuha@protonmail.com>
2021-05-04 15:21:51 +0300

    - started working on organization selection

---
+++ voting_system_mobile/lib/classes/user.dart
@@ -0,0 +1,9 @@
+class User {
+  String firstName;
+  String lastName;
+  String email;
+  String password;
+  String phoneNumber;
+
+  User({this.firstName, this.lastName, this.email, this.password, this.phoneNumber});
+}
